174cb6774971255f6ceb0947f098b799db6ec9ed
Fachri Adryansyah <155632588+fachriadr@users.noreply.github.com>
2025-10-23 16:31:17 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Myporto-react"
+  base: "/Myporto-react/"
   plugins: [react(), tailwindcss()],
 });
